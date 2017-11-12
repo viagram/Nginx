119218b32e3f260fe2319f05679a84f661b27c1c
viagram <viagram@users.noreply.github.com>
2017-11-12 19:52:48 +0800

    fix

--- install.sh
+++ install.sh
@@ -22,20 +22,20 @@ EOF
 echo -e "\033[0m"
 
 function Check_OS(){
-    if [[ -f /etc/redhat-release ]] ; then
-        if egrep -i "centos.*6\..*" /etc/redhat-release >/dev/null 2>&1 ; then
+    if [[ -f /etc/redhat-release ]]; then
+        if egrep -i "centos.*6\..*" /etc/redhat-release >/dev/null 2>&1; then
             echo 'centos6'
-        elif egrep -i "centos.*7\..*" /etc/redhat-release >/dev/null 2>&1 ; then
+        elif egrep -i "centos.*7\..*" /etc/redhat-release >/dev/null 2>&1; then
             echo 'centos7'
-        elif egrep -i "Red.*Hat.*6\..*" /etc/redhat-release >/dev/null 2>&1 ; then
+        elif egrep -i "Red.*Hat.*6\..*" /etc/redhat-release >/dev/null 2>&1; then
             echo 'redhat6'
-        elif egrep -i "Red.*Hat.*7\..*" /etc/redhat-release >/dev/null 2>&1 ; then
+        elif egrep -i "Red.*Hat.*7\..*" /etc/redhat-release >/dev/null 2>&1; then
             echo 'redhat7'
         fi
-    elif [[ -f /etc/issue ]] ; then
-        if egrep -i "debian" /etc/issue >/dev/null 2>&1 ; then
+    elif [[ -f /etc/issue ]]; then
+        if egrep -i "debian" /etc/issue >/dev/null 2>&1; then
             echo 'debian'
-        elif egrep -i "ubuntu" /etc/issue >/dev/null 2>&1 ; then
+        elif egrep -i "ubuntu" /etc/issue >/dev/null 2>&1; then
             echo 'ubuntu'
         fi
     else
@@ -50,26 +50,26 @@ function printnew(){
     HUANHANG=0
     for PARSTR in "${@}";do
         CHK="${PARSTR}"
-        if echo "${CHK}" | egrep -io "^\-[[:graph:]]*" >/dev/null 2>&1 ; then
-            if [[ "${CHK}" == "-black" ]] ; then
+        if echo "${CHK}" | egrep -io "^\-[[:graph:]]*" >/dev/null 2>&1; then
+            if [[ "${CHK}" == "-black" ]]; then
                 COLOUR="\033[30m"
-            elif [[ "${CHK}" == "-red" ]] ; then
+            elif [[ "${CHK}" == "-red" ]]; then
                 COLOUR="\033[31m"
-            elif [[ "${CHK}" == "-green" ]] ; then
+            elif [[ "${CHK}" == "-green" ]]; then
                 COLOUR="\033[32m"
-            elif [[ "${CHK}" == "-yellow" ]] ; then
+            elif [[ "${CHK}" == "-yellow" ]]; then
                 COLOUR="\033[33m"
-            elif [[ "${CHK}" == "-blue" ]] ; then
+            elif [[ "${CHK}" == "-blue" ]]; then
                 COLOUR="\033[34m"
-            elif [[ "${CHK}" == "-purple" ]] ; then
+            elif [[ "${CHK}" == "-purple" ]]; then
                 COLOUR="\033[35m"
-            elif [[ "${CHK}" == "-cyan" ]] ; then
+            elif [[ "${CHK}" == "-cyan" ]]; then
                 COLOUR="\033[36m"
-            elif [[ "${CHK}" == "-white" ]] ; then
+            elif [[ "${CHK}" == "-white" ]]; then
                 COLOUR="\033[37m"
-            elif [[ "${CHK}" == "-a" ]] ; then
+            elif [[ "${CHK}" == "-a" ]]; then
                 HUANHANG=1
-            elif [[ "${CHK}" == "-r" ]] ; then
+            elif [[ "${CHK}" == "-r" ]]; then
                 RIGHT=1
             fi
         else
@@ -77,13 +77,13 @@ function printnew(){
         fi
     done
     COUNT=$(echo -n "${WENZHI}" | wc -L)
-    if [[ ${RIGHT} -eq 1 ]] ; then
+    if [[ ${RIGHT} -eq 1 ]]; then
         tput cup $(tput lines) $[$(tput cols)-${COUNT}]
         printf "${COLOUR}%b%-${COUNT}s" "${WENZHI}"
         tput cup $(tput lines) 0
     else
         tput cup $(tput lines) 0
-        if [[ ${HUANHANG} -eq 1 ]] ; then
+        if [[ ${HUANHANG} -eq 1 ]]; then
             printf "${COLOUR}%b%-${COUNT}s" "${WENZHI}"
             tput cup $(tput lines) ${COUNT}
         else
@@ -138,9 +138,9 @@ function OptNET(){
 
 #改成北京时间
 function check_datetime(){
-    if [[ "$(Check_OS)" == "centos7" ]] ; then
+    if [[ "$(Check_OS)" == "centos7" ]]; then
         timedatectl set-timezone Asia/Shanghai
-    elif [[ "$(Check_OS)" == "centos6" ]] ; then
+    elif [[ "$(Check_OS)" == "centos6" ]]; then
         rm -rf /etc/localtime
         ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
     fi
@@ -148,7 +148,7 @@ function check_datetime(){
 }
 
 # Check If You Are Root
-if [[ $EUID -ne 0 ]] ; then
+if [[ $EUID -ne 0 ]]; then
     printnew -red "错误: 必须以root权限运行此脚本! "
     exit 1
 fi
@@ -157,7 +157,7 @@ NGINX_INPATH="/usr/local/nginx"
 CPUSU=$(cat /proc/cpuinfo | grep processor | wc -l)
 #################################################################################################################################################
 
-if [[ "$(Check_OS)" != "centos7" && "$(Check_OS)" != "centos6" && "$(Check_OS)" != "redhat7" && "$(Check_OS)" != "redhat6" ]] ; then
+if [[ "$(Check_OS)" != "centos7" && "$(Check_OS)" != "centos6" && "$(Check_OS)" != "redhat7" && "$(Check_OS)" != "redhat6" ]]; then
     printnew -red "目前仅支持CentOS6-7及Redhat6-7系统."
     exit 1
 fi
@@ -171,18 +171,18 @@ if [[ ${is_go} != "y" && ${is_go} != "Y" ]]; then
 fi
 
 printnew -green "安装基础依懒软件包..."
-yum groupinstall -y 'Development Tools';
-yum install -y gcc gcc-c++ epel-release kernel-devel unzip automake make zlib-devel openssl openssl-devel pcre-devel pam-devel curl wget net-tools;
+yum groupinstall -y "Development Tools"
+yum install -y gcc gcc-c++ epel-release kernel-devel unzip automake make zlib-devel openssl openssl-devel pcre-devel pam-devel curl wget
 yum -y install libtool libevent gettext-devel ntpdate
 
 cur_dir=${PWD}/nginx_install
-if [[ -d "${cur_dir}" ]] ; then
+if [[ -d "${cur_dir}" ]]; then
     rm -rf ${cur_dir}
 fi
 mkdir -p ${cur_dir} && cd $cur_dir
 
 printnew -green "克隆libmaxminddb源码..."
-if ! git clone --recursive https://github.com/maxmind/libmaxminddb ; then
+if ! git clone --recursive https://github.com/maxmind/libmaxminddb; then
     printnew -red "克隆libmaxminddb源码失败."
     exit 1
 fi
@@ -190,15 +190,15 @@ printnew -green "编译和安装libmaxminddb..."
 cd libmaxminddb
 ./bootstrap
 ./configure
-if ! make ; then
+if ! make; then
     printnew -red "编译失败."
     exit 1
 fi
-if ! make install ; then
+if ! make install; then
     printnew -red "安装失败."
     exit 1
 fi
-if ! egrep -i "/usr/local/lib" /etc/ld.so.conf.d/local.conf >/dev/null 2>&1 ; then
+if ! egrep -i "/usr/local/lib" /etc/ld.so.conf.d/local.conf >/dev/null 2>&1; then
     echo "/usr/local/lib">>/etc/ld.so.conf.d/local.conf
 fi
 ldconfig >/dev/null 2>&1
@@ -207,17 +207,17 @@ cd ..
 printnew -green "获取nginx信息..."
 DOWN=$(curl -sk http://nginx.org/en/download.html | egrep -io '<h4>Stable version</h4>[[:print:]]*<h4>Legacy versions</h4>' | egrep -io '/download/nginx-[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}\.tar\.g[z$]' | head -n 1)
 NAME=$(echo ${DOWN} | egrep -io 'nginx-[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}')
-if [[ -z ${NAME} ]] ; then
+if [[ -z ${NAME} ]]; then
     printnew -red "获取nginx信息失败."
     exit 1
 fi
 
 printnew -green "下载nginx源码..."
-if ! wget -O ${NAME}.tar.gz -c http://nginx.org${DOWN} --no-check-certificate ; then
+if ! wget -O ${NAME}.tar.gz -c http://nginx.org${DOWN} --no-check-certificate; then
     printnew -red "下载nginx源码失败."
     exit 1
 fi
-if ! tar zxf ${NAME}.tar.gz ; then
+if ! tar zxf ${NAME}.tar.gz; then
     printnew -red "解压nginx源码失败."
     exit 1
 fi
@@ -225,45 +225,45 @@ cd ${NAME}
 
 printnew -green "下载和克隆nginx组件源码..."
 #git clone https://github.com/yaoweibin/nginx_tcp_proxy_module.git
-if ! git clone https://github.com/leev/ngx_http_geoip2_module.git ; then
+if ! git clone https://github.com/leev/ngx_http_geoip2_module.git; then
     printnew -red "克隆ngx_http_geoip2_module源码失败."
     exit 1
 fi
-if ! git clone https://github.com/cuber/ngx_http_google_filter_module.git ; then
+if ! git clone https://github.com/cuber/ngx_http_google_filter_module.git; then
     printnew -red "克隆ngx_http_google_filter_module源码失败."
     exit 1
 fi
-if ! git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git ; then
+if ! git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git; then
     printnew -red "克隆ngx_http_substitutions_filter_module源码失败."
     exit 1
 fi
 PCRE_URL=$(curl -sk https://ftp.pcre.org/pub/pcre/ | egrep -io 'pcre-[0-9]{1,2}.[0-9]{1,2}.tar.gz' | awk 'END{print "https://ftp.pcre.org/pub/pcre/"$0}')
 PCRE_NAME=$(echo ${PCRE_URL} | egrep -io 'pcre-[0-9]{1,2}.[0-9]{1,2}')
-if ! wget -O ${PCRE_NAME}.tar.gz -c ${PCRE_URL} --no-check-certificate ; then
+if ! wget -O ${PCRE_NAME}.tar.gz -c ${PCRE_URL} --no-check-certificate; then
     printnew -red "下载pcre源码失败."
     exit 1
 fi
-if ! tar zxf ${PCRE_NAME}.tar.gz ; then
+if ! tar zxf ${PCRE_NAME}.tar.gz; then
     printnew -red "解压pcre源码失败."
     exit 1
 fi
 OPENSSL_URL=$(curl -sk https://www.openssl.org/source/ | egrep -io 'openssl-[0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2}[a-z]{1}.tar.gz' | head -n 1 | awk '{print "https://www.openssl.org/source/"$0}')
 OPENSSL_NAME=$(echo ${OPENSSL_URL} | egrep -io 'openssl-[0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2}[a-z]{1}')
-if ! wget -O ${OPENSSL_NAME}.tar.gz -c ${OPENSSL_URL} --no-check-certificate ; then
+if ! wget -O ${OPENSSL_NAME}.tar.gz -c ${OPENSSL_URL} --no-check-certificate; then
     printnew -red "下载openssl源码失败."
     exit 1
 fi
-if ! tar zxf ${OPENSSL_NAME}.tar.gz ; then
+if ! tar zxf ${OPENSSL_NAME}.tar.gz; then
     printnew -red "解压openssl源码失败."
     exit 1
 fi
 ZLIB_URL=$(curl -sk https://zlib.net/ | egrep -io 'zlib-([0-9]{1,2}.){3}tar.gz' | head -n 1 | awk '{print "https://zlib.net/"$0}')
 ZLIB_NAME=$(echo ${ZLIB_URL} | egrep -io 'zlib-[0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2}')
-if ! wget -O ${ZLIB_NAME}.tar.gz -c ${ZLIB_URL} --no-check-certificate ; then
+if ! wget -O ${ZLIB_NAME}.tar.gz -c ${ZLIB_URL} --no-check-certificate; then
     printnew -red "下载zlib源码失败."
     exit 1
 fi
-if ! tar zxf ${ZLIB_NAME}.tar.gz ; then
+if ! tar zxf ${ZLIB_NAME}.tar.gz; then
     printnew -red "解压zlib源码失败."
     exit 1
 fi
@@ -286,11 +286,11 @@ printnew -green "编译和安装Nginx..."
     --add-module=ngx_http_google_filter_module \
     --add-module=ngx_http_substitutions_filter_module \
     --prefix=${NGINX_INPATH}
-if ! make ; then
+if ! make; then
     printnew -red "编译失败."
     exit 1
 fi
-if ! make install ; then
+if ! make install; then
     printnew -red "安装失败."
     exit 1
 fi
@@ -298,19 +298,19 @@ cd ..
 
 printnew -green "下载GeoLite2-Country.mmdb..."
 [[ -f ${NGINX_INPATH}/GeoLite2-Country.mmdb.gz ]] && rm -f ${NGINX_INPATH}/GeoLite2-Country.mmdb.gz
-if ! wget -O ${NGINX_INPATH}/GeoLite2-Country.mmdb.gz -c http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz --no-check-certificate ; then
+if ! wget -O ${NGINX_INPATH}/GeoLite2-Country.mmdb.gz -c http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz --no-check-certificate; then
     printnew -red "下载GeoLite2-Country.mmdb失败."
     exit 1
 fi
-if ! gunzip -f ${NGINX_INPATH}/GeoLite2-Country.mmdb.gz ; then
+if ! gunzip -f ${NGINX_INPATH}/GeoLite2-Country.mmdb.gz; then
     printnew -red "解压GeoLite2-Country.mmdb失败."
     exit 1
 fi
 rm -f ${NGINX_INPATH}/GeoLite2-Country.mmdb.gz
 
 printnew -green "下载和安装Nginx服务..."
-if [[ "$(Check_OS)" == "centos6" || "$(Check_OS)" == "redhat6" ]] ; then
-    if ! wget -O nginx -c  https://raw.githubusercontent.com/viagram/Nginx/master/CentOS-6 --no-check-certificate ; then
+if [[ "$(Check_OS)" == "centos6" || "$(Check_OS)" == "redhat6" ]]; then
+    if ! wget -O nginx -c  https://raw.githubusercontent.com/viagram/Nginx/master/CentOS-6 --no-check-certificate; then
         printnew -red "下载Nginx服务配置失败."
         exit 1
     fi
@@ -319,8 +319,8 @@ if [[ "$(Check_OS)" == "centos6" || "$(Check_OS)" == "redhat6" ]] ; then
     chmod 775 /etc/init.d/nginx >/dev/null 2>&1
     chkconfig --add nginx  >/dev/null 2>&1
     chkconfig nginx on >/dev/null 2>&1
-elif [[ "$(Check_OS)" == "centos7" || "$(Check_OS)" == "redhat7" ]] ; then
-    if ! wget -O nginx.service -c  https://raw.githubusercontent.com/viagram/Nginx/master/CentOS-7 --no-check-certificate ; then
+elif [[ "$(Check_OS)" == "centos7" || "$(Check_OS)" == "redhat7" ]]; then
+    if ! wget -O nginx.service -c  https://raw.githubusercontent.com/viagram/Nginx/master/CentOS-7 --no-check-certificate; then
         printnew -red "下载Nginx服务配置失败."
         exit 1
     fi
@@ -330,29 +330,29 @@ elif [[ "$(Check_OS)" == "centos7" || "$(Check_OS)" == "redhat7" ]] ; then
     systemctl enable nginx.service
     systemctl daemon-reload
 fi
-if ! wget -O 404.html -c  https://raw.githubusercontent.com/viagram/Nginx/master/404.html --no-check-certificate ; then
+if ! wget -O 404.html -c  https://raw.githubusercontent.com/viagram/Nginx/master/404.html --no-check-certificate; then
     printnew -red "下载404.html失败."
 fi
 \cp -f 404.html ${NGINX_INPATH}/html/404.html
-if ! wget -O index.html -c  https://raw.githubusercontent.com/viagram/Nginx/master/index.html --no-check-certificate ; then
+if ! wget -O index.html -c  https://raw.githubusercontent.com/viagram/Nginx/master/index.html --no-check-certificate; then
     printnew -red "下载index.html失败."
 fi
 \cp -f index.html ${NGINX_INPATH}/html/index.html
 if ps -ef | grep -v grep | grep -i 'php-fpm'; then
-    if ! wget -O nginx.conf -c  https://raw.githubusercontent.com/viagram/Nginx/master/nginx.conf --no-check-certificate ; then
+    if ! wget -O nginx.conf -c  https://raw.githubusercontent.com/viagram/Nginx/master/nginx.conf --no-check-certificate; then
         printnew -red "下载nginx.conf失败."
     fi
 else
-    if ! wget -O nginx.conf -c  https://raw.githubusercontent.com/viagram/Nginx/master/nginx-php.conf --no-check-certificate ; then
+    if ! wget -O nginx.conf -c  https://raw.githubusercontent.com/viagram/Nginx/master/nginx-php.conf --no-check-certificate; then
         printnew -red "下载nginx.conf失败."
     fi
 fi
 sed -i "s/CPUSU/${CPUSU}/g" nginx.conf
 \cp -f nginx.conf ${NGINX_INPATH}/conf/nginx.conf
 
-if [[ "$(Check_OS)" == "centos6" || "$(Check_OS)" == "redhat6" ]] ; then
+if [[ "$(Check_OS)" == "centos6" || "$(Check_OS)" == "redhat6" ]]; then
     service nginx restart
-elif [[ "$(Check_OS)" == "centos7" || "$(Check_OS)" == "redhat7" ]] ; then
+elif [[ "$(Check_OS)" == "centos7" || "$(Check_OS)" == "redhat7" ]]; then
     systemctl start nginx
 fi
 

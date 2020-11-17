d26e36985dceecc6f1a54a902c26662bd2bc62c6
viagram <viagram@users.noreply.github.com>
2020-11-17 18:18:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -319,6 +319,7 @@ if ! make install; then
 	exit 1
 fi
 cd ..
+ln -f /usr/local/nginx/sbin/nginx /usr/sbin/nginx
 
 printnew -green "下载GeoLite2-Country.mmdb..."
 GeoLite2_Name='GeoLite2-Country.tar.gz'
